82f2e3be9511007933a008820a5edf110a1ef2d5
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-06-16 16:49:31 +0900

    コマンドライン引数の解析

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,17 @@ void printUsage(const std::string& programName)
 	std::cout << "  " << programName << "                    # Use default configuration" << std::endl;
 	std::cout << "  " << programName << " my_config.conf     # Use custom configuration" << std::endl;
 }
+void printVersion()
+{
+	std::cout << "Webserv 1.0" << std::endl;
+	std::cout << "HTTP/1.1 compliant web server implementation" << std::endl;
+	std::cout << "Built with C++98 standard" << std::endl;
+}
 
+void printWelcome()
+{
+	std::cout << "Welcome to Webserv" << std::endl;
+}
 int main (int argc, char* argv[])
 {
 	std::string configPath = DEFAULT_CONFIG;
@@ -26,13 +36,23 @@ int main (int argc, char* argv[])
 		std::string arg = argv[1];
 		if (arg == "--help" || arg == "-h")
 		{
+			// ヘルプメッセージの表示
 			printUsage(argv[0]);
 			return 0;
 		}
-		else if()
+		else if(arg == "--version" || arg == "-v")
 		{
-
+			printVersion();
+			return 0;
 		}
+		else
+			configPath = arg;
+	}
 
+	if (argc > 2)
+	{
+		std::cerr << "Error:Too many arguments" << std::endl;
+		printUsage(argv[0]);
+		return 1;
 	}
 }
