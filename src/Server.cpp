2cc64d2c66fff787c1fb80ad8956e9e844bc382e
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-07-01 20:03:38 +0900

    Serverクラスの最小限の実装

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,5 +1,35 @@
 # include "../inc/Server.hpp"
+# include "../inc/CGI.hpp"
 
-Server::Server() : _config(NULL), _running(false){
+// Static member initialization
+Server* Server::instance = NULL;
+
+Server::Server() : _config(NULL), _running(false) {
 	instance = this;
+}
+
+Server::Server(const std::string& configPath) : _config(NULL), _running(false) {
+	instance = this;
+}
+
+Server::~Server() {
+	stop();
+	delete _config;
+	instance = NULL;
+}
+
+bool Server::initialize() {
+
+	return true;
+}
+
+void Server::run() {
+	_running = true;
+	while (_running) {
+
+	}
+}
+
+void Server::stop() {
+	_running = false;
 }
